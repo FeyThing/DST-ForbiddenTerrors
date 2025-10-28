54391c17a954af87a713cd69971eb45ad0e9b2fb
HalfEnder776 <64817682+HalfEnder776@users.noreply.github.com>
2025-10-27 23:01:25 -0400

    -- Disabled overtime sanity affecting ichor

--- postinit/components/sanity.lua
+++ postinit/components/sanity.lua
@@ -4,9 +4,9 @@ GLOBAL.setfenv(1, GLOBAL)
 local Sanity = require("components/sanity")
 
 local _DoDelta = Sanity.DoDelta
-function Sanity:DoDelta(delta, ...)
-    if delta ~= nil and delta > 0 then
+function Sanity:DoDelta(delta, overtime, ...)
+    if delta ~= nil and delta > 0 and not overtime then
         self.inst.components.df_ichormanager:DoDelta(-delta * TUNING.DF_ICHOR_SANITY_DELTA_MULT) 
     end
-    return _DoDelta(self, delta, ...)
+    return _DoDelta(self, delta, overtime, ...)
 end
